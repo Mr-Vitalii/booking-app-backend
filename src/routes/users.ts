2c6d3b5ed6317ed047f5ec84bb723f57698722bd
Mr-Vitalii <co4ckowoi.v@gmail.com>
2024-01-26 20:21:39 +0200

    implemented registration logic

---
+++ src/routes/users.ts
@@ -0,0 +1,8 @@
+import express, { Request, Response } from "express";
+const ctrl = require("../controllers/user");
+
+const router = express.Router();
+
+router.post("/register", ctrl.registerValidators, ctrl.register);
+
+export default router;
