1e15bc329b24901dab43affdf3868d9855943e7b
Mr-Vitalii <co4ckowoi.v@gmail.com>
2024-01-31 17:19:37 +0200

    deleted BookingType

--- dist/middleware/auth.js
+++ dist/middleware/auth.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.verifyToken = void 0;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const verifyToken = (req, res, next) => {
     const token = req.cookies["auth_token"];
@@ -18,4 +19,4 @@ const verifyToken = (req, res, next) => {
         return res.status(401).json({ message: "unauthorized" });
     }
 };
-exports.default = verifyToken;
+exports.verifyToken = verifyToken;
