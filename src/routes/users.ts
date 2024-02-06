482fbd342d8578d8e3b946f9c400aa25c62294e2
Mr-Vitalii <co4ckowoi.v@gmail.com>
2024-02-06 19:12:00 +0200

    created route and controller for feaching current user

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import * as ctrl from "../controllers/user";
+import { verifyToken } from "../middleware/auth";
 
 const router = express.Router();
+
+router.get("/me", verifyToken, ctrl.getCurrentUser);
 
 router.post("/register", ctrl.registerValidators, ctrl.register);
 
